39e7cd122338936a24169539574e6bcff8352fce
Jan Stepanek <jan.stepanek@matfyz.cz>
2020-01-20 23:09:08 +0100

    Add script to compare PDTSC w-data with old files token by token
    The following files seem to be different:
    
    ak_046 pdtsc_093_3
    ak_051 pdtsc_128_3
    ak_140 pdtsc_054_3
    ep_107 pdtsc_103_2
    ez_061 pdtsc_091_3
    ez_122 pdtsc_100_3
    ez_144 pdtsc_058_3
    hg_028 pdtsc_121_2
    hg_714 pdtsc_017_2
    hg_991 pdtsc_034_2
    hs_116 pdtsc_048_2
    jg_057 pdtsc_022_1
    lk_016 pdtsc_046_1
    lk_037 pdtsc_085_1
    ml_142 pdtsc_092_2
    pk_018 pdtsc_132_2
    pk_143 pdtsc_074_3
    ps_003 pdtsc_099_3
    ps_033 pdtsc_082_2

---
+++ WorkData/tools/pdtsc-map-nodes.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature qw{ say };
+
+use FindBin;
+
+use open IO => ':encoding(UTF-8)', ':std';
+
+my $orig_dir = "$FindBin::Bin/../../OriginalInputData/PDTSC/data";
+
+{   package Local::Original::Iterator;
+    use Moo;
+    has file => (is => 'ro');
+    has _fh => (is => 'ro', lazy => 1, builder => '_build_fh');
+    sub _build_fh {
+        my ($self) = @_;
+        open my $fh, '<', "$orig_dir/" . $self->file . ".mdata"
+            or die $self->file, ": $!";
+        return $fh
+    }
+    sub next_token {
+        my ($self) = @_;
+        my $line = "";
+        $line = readline $self->_fh
+            until eof $self->_fh || $line =~ m{<form>(.*)</form>};
+
+        my $value = $1;
+        return $value || ""
+    }
+    sub done { eof $_[0]->_fh }
+}
+
+{   package Local::New::Iterator;
+    use Moo;
+    has file => (is => 'ro');
+    has _num => (is => 'rw', default => '00');
+    has _fh => (is => 'ro', lazy => 1, builder => '_build_fh',
+                clearer => '_clear_fh');
+    sub _build_fh {
+        my ($self) = @_;
+        open my $fh, '<', "$orig_dir/" . $self->file . '.' . $self->_num . '.w'
+            or return 'done';
+        return $fh
+    }
+    sub next_token {
+        my ($self) = @_;
+        my ($line, $value) = ("");
+        return "" if $self->done;
+
+        until ($value = ($line =~ m{<token>(.*)</token>})[0]
+               or eof $self->_fh
+         ) {
+            $line = readline $self->_fh;
+            if (eof $self->_fh) {
+                $self->_num(sprintf '%02d', 1 + $self->_num);
+                $self->_clear_fh;
+            }
+        }
+        return $value || ""
+    }
+    sub done { 'done' eq $_[0]->_fh }
+}
+
+
+while (<>) {
+    my ($new, $old) = split;
+    my $old_i = 'Local::Original::Iterator'->new(file => $old);
+    my $new_i = 'Local::New::Iterator'->new(file => $new);
+
+    my $same = 1;
+    until ($old_i->done || $new_i->done) {
+        my ($o, $n) = map $_->next_token, $old_i, $new_i;
+        undef $same unless $o eq $n;
+    }
+    say "$new $old" unless $same;
+}
+
+# Expects output of identify_pdtsc.pl as input.
