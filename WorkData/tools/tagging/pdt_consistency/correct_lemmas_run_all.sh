9f762c0b0c6509582282d55601010bcb68f4288b
Milan Straka <milan@strakovi.com>
2020-04-16 10:55:48 +0200

    Generate logs with .txt extension.

--- WorkData/tools/tagging/pdt_consistency/correct_lemmas_run_all.sh
+++ WorkData/tools/tagging/pdt_consistency/correct_lemmas_run_all.sh
@@ -5,7 +5,7 @@ for d in Faust PCEDT PDT PDTSC; do
   (
     cd $d
     [ -n "$TSV_VALID" ] || python3 ../pdtc_to_vertical.py `find ../../../../$d -name "*.m"` >$d.tsv
-    python3 ../correct_lemmas.py $d.tsv | sort | uniq -c >$d.correctness.log
+    python3 ../correct_lemmas.py $d.tsv | sort | uniq -c >$d.correctness.txt
   ) &
 done
 wait
