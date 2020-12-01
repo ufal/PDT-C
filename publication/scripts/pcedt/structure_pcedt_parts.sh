7589eacc676c662bbe00f29e589b68553f36d756
Michal Novak <mnovak@ufal.mff.cuni.cz>
2020-12-02 00:02:45 +0100

    taking en and cs parts and joining them to the treex file

---
+++ publication/scripts/pcedt/structure_pcedt_parts.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+src_en_dir=$1
+src_cs_dir=$2
+trg_dir=$3
+
+mkdir -p $trg_dir
+for j in `find $src_cs_dir -name 'wsj*.[wmat]'`; do
+    src_file=`basename $j`;
+    src_base=`echo $src_file | sed 's/\..*$//'`;
+    trg_file=`echo $src_file | sed 's/\.cz\./.cs./'`;
+    trg_base=`echo $trg_file | sed 's/\..*$//'`;
+    echo $j "--->" $trg_dir/$trg_file.gz;
+    cat $j | \
+        sed "s/$src_base\.cz\.\([atmw]\)/$trg_base.cs.\1.gz/g" | \
+        gzip -c > $trg_dir/$trg_file.gz;
+done;
+for j in `find $src_en_dir -name 'wsj*.[pat].gz'`; do
+    src_file=`basename $j`;
+    src_base=`echo $src_file | sed 's/\..*$//'`;
+    trg_file=`echo $src_file | sed 's/^wsj_\(....\)\.\(.\)\.gz$/wsj\1.en.\2.gz/'`;
+    trg_base=`echo $trg_file | sed 's/\..*$//'`;
+    echo $j "--->" $trg_dir/$trg_file;
+    zcat $j | \
+        sed "s/$src_base\.\([aptmw]\)\.gz/$trg_base.en.\1.gz/g" | \
+        sed 's/target-node\.rf/target_node.rf/g' | \
+        sed 's/informal-type/type/g' | \
+        gzip -c > $trg_dir/$trg_file;
+done;
