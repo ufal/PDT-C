817a0907d2d2e91a87fbf03f8b6f89295d0bc9f2
Jiri Mirovsky <mirovsky@ufal.mff.cuni.cz>
2021-02-11 16:27:21 +0100

    Workaround for checking links from a-files to p-files in PCEDT-en

--- publication/scripts/56_check_internal_links_start.sh
+++ publication/scripts/56_check_internal_links_start.sh
@@ -12,10 +12,13 @@ mv -f $SUM $SUM.old
 
 CORP="PCEDT-en"
 
-echo "Checking internal links in $CORP"
-echo "Checking internal links in $CORP" >>$LOG
+echo "Checking links in t-files in $CORP"
+echo "Checking links in t-files in $CORP" >>$LOG
 btred -I 56_check_internal_links.btred $BASE/$TGT/*/*.t.gz >>$LOG 2>&1
-#btred -I 56_check_p_links.btred $BASE/$TGT/*/*.a.gz >>$LOG 2>&1
+
+echo "Checking links in a-files in $CORP"
+echo "Checking links in a-files in $CORP" >>$LOG
+btred -I 56_check_p_links.btred $BASE/$TGT/*/*.a.gz >>$LOG 2>&1
 
 grep -v Saving $LOG | grep -v Processing | grep -v PDTB | grep -v PMLTQ | grep -v Subroutine | grep -v Applying | grep -v Initializing | cut -f 1,2,3 -d " " | sort | uniq -c | tee $SUM
 
