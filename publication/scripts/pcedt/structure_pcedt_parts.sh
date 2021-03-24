b6ea0531905a411f78141356d1d4b3d3af09fc67
Michal Novak <mnovak@ufal.mff.cuni.cz>
2021-03-24 19:43:39 +0100

    source PDT-C files are gzipped

--- publication/scripts/pcedt/structure_pcedt_parts.sh
+++ publication/scripts/pcedt/structure_pcedt_parts.sh
@@ -5,15 +5,15 @@ src_cs_dir=$2
 trg_dir=$3
 
 mkdir -p $trg_dir
-for j in `find $src_cs_dir -name 'wsj*.[wmat]' | sort`; do
+for j in `find $src_cs_dir -name 'wsj*.[wmat].gz' | sort`; do
     src_file=`basename $j`;
     src_base=`echo $src_file | sed 's/\..*$//'`;
     trg_file=`echo $src_file | sed 's/\.cz\./.cs./'`;
     trg_base=`echo $trg_file | sed 's/\..*$//'`;
-    echo $j "--->" $trg_dir/$trg_file.gz;
-    cat $j | \
-        sed "s/$src_base\.cz\.\([atmw]\)/$trg_base.cs.\1.gz/g" | \
-        gzip -c > $trg_dir/$trg_file.gz;
+    echo $j "--->" $trg_dir/$trg_file;
+    zcat $j | \
+        sed "s/$src_base\.cz\.\([atmw]\)/$trg_base.cs.\1/g" | \
+        gzip -c > $trg_dir/$trg_file;
 done;
 for j in `find $src_en_dir -name 'wsj*.[pat].gz' | sort`; do
     src_file=`basename $j`;
