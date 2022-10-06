2217370451e6e7df52f9f8692d88ec5e98d45674
Jan Stepanek <jan.stepanek@matfyz.cz>
2022-10-07 00:27:44 +0200

    Add scripts to generate "templates"
    It should find the same phrases annotated differently, i.e. with
    different afuns.
    
    Currently, if the afuns are the same but structures are different, the
    phrase is ignored, but it should be pretty rare.

---
+++ WorkData2.0/scripts/templates.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+set -eu
+export LC_ALL=C
+
+if (( $# == 0 )) ; then
+    cat <<-EOF
+	Usage: $0 path_to_pdt-c
+	EOF
+    exit 1
+fi
+
+pdt_c_dir=$1
+
+function nsort() {
+    sort | uniq -c | sort -n
+}
+
+if [[ ! -f templates.o ]] ; then
+    time btred -qI "$pdt_c_dir"/WorkData2.0/scripts/templates.btred annotators/???/done/wsj*.cz.a > templates.o
+fi
+
+cut -f1,4 templates.o | nsort | grep -v '^ *1 ' > templates.no_afun
+cut -f1,2,4 templates.o | nsort | grep -v '^ *1 ' | cut -f1,3 > templates.afuns
+comm -3 <(sort templates.afuns) <(sort templates.no_afun) \
+| sed 's/^[ \t]*[0-9]*//' \
+| nsort \
+| sed $'s/^[ \t]*[0-9]*  //' \
+| cut -f1 \
+| perl -E '
+use warnings;
+use strict;
+my %lines;
+while (<>) {
+    chomp;
+    undef $lines{$_};
+}
+open my $in, "<", "templates.o" or die $!;
+while (<$in>) {
+    chomp;
+    my ($s) = /(.*?)\t/;
+    say if exists $lines{$s};
+}
+' | sort -t$'\t' -k4,4rn -k1,1 -k3,3 \
+| perl -E '
+use warnings;
+use strict;
+my $previous = "";
+while (<>) {
+    my $phrase = (split /\t/, $_)[0];
+    if ($phrase ne $previous) {
+        say "\n$phrase";
+        $previous = $phrase;
+    }
+    s/.*?\t//;
+    s{\t[0-9]+\t/.*pdtc2a/}{\t};
+    print;
+}
+'
+rm templates.{no_afun,afuns}
