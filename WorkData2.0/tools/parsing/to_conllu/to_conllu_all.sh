8a7d631c38e3f8d59931ff9bc249a3cfa2cae845
Milan Straka <milan@strakovi.com>
2022-11-03 22:54:33 +0100

    Update script to slurm.

--- WorkData2.0/tools/parsing/to_conllu/to_conllu_all.sh
+++ WorkData2.0/tools/parsing/to_conllu/to_conllu_all.sh
@@ -2,10 +2,10 @@
 
 [ $# -ge 1 ] || { echo "Usage: $0 treebank [N]" >&2; exit 1; }
 treebank="$1"
-N=${2:-250}
+N=${2:-200}
 
 for layer in a; do #m
   for i in $(seq 1 $N); do
-    qsub -q cpu-troja.q -o /dev/null -j y bash $(dirname $0)/to_conllu.sh $treebank $layer $(split -n l/$i/$N files_$layer)
+    sbatch -p cpu-ms run bash $(dirname $0)/to_conllu.sh $treebank $layer $(split -n l/$i/$N files_$layer)
   done
 done
