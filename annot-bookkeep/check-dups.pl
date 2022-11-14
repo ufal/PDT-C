e27e6fbc2d9e9c46602f4e5581c278e40176d269
Jan Stepanek <jan.stepanek@matfyz.cz>
2022-11-15 00:11:01 +0100

    Report surplus duplicate files, too

--- annot-bookkeep/check-dups.pl
+++ annot-bookkeep/check-dups.pl
@@ -17,23 +17,27 @@ $list->for_each(sub {
         my ($part) = $file->name =~ /wsj(....)/;
         return unless exists $mask{$part};
 
-        my $afile;
         my $workdir = $list->workdir($file->annotator);
+        my %afile = (kept => "$workdir/done/",
+                     dups => join("", $workdir, '/../../duplicates/',
+                                      $file->annotator, '/'),
+                     todo => "$workdir/");
+        my $type;
         if ($file->done) {
             if ($file->comment =~ /kept/) {
-                print 'kept';
-                $afile = "$workdir/done/";
+                $type = 'kept';
             } else {
-                print 'dups';
-                $afile = join "", $workdir, '/../../duplicates/',
-                         $file->annotator, '/';
+                $type = 'dups';
             }
         } else {
-            print 'todo';
-            $afile = "$workdir/";
+            $type = 'todo';
         }
-        $afile .= "$mask{$part}.cz.a";
-        say "\t", (-f "$afile") ? 'OK' : 'Missing', "\t", $afile;
+        my $file = $afile{$type} . "$mask{$part}.cz.a";
+        say "$type\t", (-f $file) ? 'OK' : 'Missing', "\t", $file;
+        for my $other (grep $_ ne $type, sort keys %afile) {
+            my $file = $afile{$other} . "$mask{$part}.cz.a";
+            say "sur+\tSur+\t$file" if -e $file;
+        }
     } else {
         $mask{$file->comment} = $file->name;
     }
