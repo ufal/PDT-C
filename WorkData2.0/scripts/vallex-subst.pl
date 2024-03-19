12b2e186c18dea90b7cf4bebfb4120ca1dc95298
Jan Stepanek <jan.stepanek@matfyz.cz>
2024-03-19 21:51:40 +0100

    Add scripts to find obsolete frames in the vallex and data

---
+++ WorkData2.0/scripts/vallex-subst.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature qw{ say };
+
+use List::Util qw{ uniq };
+use XML::LibXML;
+
+my $vallex = 'XML::LibXML'->load_xml(location => shift);
+
+my %frame;
+sub by_id {
+    my ($id) = @_;
+    return $frame{$id}
+}
+
+sub is_valid {
+    my ($frame) = @_;
+    $frame->{status} =~ /^active$|^reviewed$|^new-(?:complete|form)$/ ? 1 : 0;
+}
+
+sub vs {
+    my ($id) = @_;
+
+    my (@frames) = by_id($id);
+    my @resolve;
+    my %resolved;
+    while (@resolve = grep $_->{status} eq 'substituted', @frames) {
+        @resolved{ map $_->{id}, @resolve } = ();
+        @frames = uniq(grep ! exists $resolved{ $_->{id} },
+                       @frames,
+                       map by_id($_),
+                       map { split / /, $_->{substituted_with} }
+                       @resolve);
+        say "\t$id -> ", join ', ', sort map $_->{id}, @frames;
+    }
+    my @valid = grep is_valid($_), @frames;
+    if (@valid != 1) {
+        say "! $id: ", join ', ', map $_->{id}, @valid;
+    } elsif ($frames[0]{id} ne $id) {
+        say "$id -> ", $valid[0]{id}, " ($valid[0]{status})";
+    } else {
+        say "ok $id ($valid[0]{status})";
+    }
+}
+
+$frame{ $_->{id} } = $_ for $vallex->findnodes('//frame');
+vs($_) for sort keys %frame;
