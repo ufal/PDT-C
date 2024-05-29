9438355df7043969547ef10cf9981a95668a4b7e
Jan Stepanek <jan.stepanek@matfyz.cz>
2024-05-29 15:03:24 +0200

    Fix both ord in a-files and deepord in t-files based on doc element

--- WorkData2.0/scripts/fix-ord.pl
+++ WorkData2.0/scripts/fix-ord.pl
@@ -7,6 +7,9 @@ use XML::LibXML;
 
 use constant PML => 'http://ufal.mff.cuni.cz/pdt/pml/';
 
+my %ORD = (tdata => 'deepord',
+           adata => 'ord');
+
 my $xpc = 'XML::LibXML::XPathContext'->new;
 $xpc->registerNs(pml => PML);
 
@@ -15,27 +18,28 @@ for my $tfile (@ARGV) {
     my $changed = 0;
     print {*STDERR} $count++, "/", scalar @ARGV, " $tfile \r";
     my $dom = 'XML::LibXML'->load_xml(location => $tfile);
-    my @roots = $xpc->findnodes('/pml:tdata/pml:trees/*', $dom);
+    my $doc_el = $dom->documentElement->nodeName;
+    my @roots = $xpc->findnodes("/pml:$doc_el/pml:trees/*", $dom);
     warn "\nNo roots" unless @roots;
 
     for my $root (@roots) {
-        my @deepords = sort { $a->textContent <=> $b->textContent }
-                       $xpc->findnodes('.//pml:deepord', $root);
-        my @ords = map $_->textContent, @deepords;
+        my @sorted_ords = sort { $a->textContent <=> $b->textContent }
+                          $xpc->findnodes(".//pml:$ORD{$doc_el}", $root);
+        my @ords = map $_->textContent, @sorted_ords;
 
         if ($ords[0] != 0) {
             warn "\nInsert zero";
             unshift @ords, 0;
-            my $zero = $root->addNewChild(PML, 'deepord');
+            my $zero = $root->addNewChild(PML, $ORD{$doc_el});
             $zero->addChild($dom->createTextNode('0'));
             $changed = 1;
         }
 
         if ($ords[-1] != $#ords) {
             warn "\n", $root->{id}, ": @ords";
-            for my $i (0 .. $#deepords) {
-                $deepords[$i]->removeChildNodes;
-                $deepords[$i]->addChild($dom->createTextNode($i));
+            for my $i (0 .. $#sorted_ords) {
+                $sorted_ords[$i]->removeChildNodes;
+                $sorted_ords[$i]->addChild($dom->createTextNode($i));
             }
             $changed = 1;
         }
